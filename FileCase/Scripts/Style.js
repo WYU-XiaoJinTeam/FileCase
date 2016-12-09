7b0a3b36b88fad66fb507ae07b9ed1e944fee2cc
rainy <904616565@qq.com>
2016-12-09 14:58:16 +0800

    111

--- FileCase/Scripts/Style.js
+++ FileCase/Scripts/Style.js
@@ -42,11 +42,6 @@
         }
     }
 
-    //文件树打开延时
-    $(function () {
-        $(".tree").treemenu({ delay: 300 }).openActive();
-    });
-
     $(document).ready(function () {
 
         //列表显示和图标显示切换
@@ -62,31 +57,6 @@
             $("#public_List").css('display', 'none');
             $("#public_Img").css('display', 'inline');
         });
-
-        //右键点击事件
-       /* $('.tree').bind("contextmenu", function (e) {   //限制鼠标右键点击
-            return false;
-        });
-        $('.tree').children().contextMenu('myMenu1', {
-            bindings: {
-                'new': function (t) {
-                    alert('Trigger was ' + t.id + '\nAction was Open');
-                },
-                'privilege': function (t) {
-                    alert('Trigger was ' + t.id + '\nAction was pricilege');
-                },
-                'rename': function (t) {
-                    alert('Trigger was ' + t.id + '\nAction was Email');
-                },
-                'trash': function (t) {
-                    alert('Trigger was ' + t.id + '\nAction was Save');
-                },
-                'delete': function (t) {
-                    alert('Trigger was ' + t.id + '\nAction was Delete');
-                }
-            }
-        });*/
-
         //选中相片div
         $('.photo').click(function () {
             $(this).toggleClass('choosepicture');
@@ -99,18 +69,61 @@
             });
             if ($("input[name='checkAll']").length == 0) {
                 document.all.selAll.checked = false;
-            }
-            /*if( $("input[name='checkAll']:checked").length==0)
-            {
-                 document.getElementById("showandhide").style.display="none";
-            }*/
-        });
-
-        //上传文件插件
-        $('#ssi-upload').ssi_uploader({
-            url: 'filecase/new_file.html', maxFileSize: 6, allowed: ['jpg', 'jpeg', 'gif', 'txt', 'png', 'pdf', 'zip', 'doc', 'docx', 'xls', 'ppt', 'pptx', 'psd']
+            }   
         });
     })
+
+
+    $(function () {
+        var show_per_page = 2;//每页5项
+        var number_of_items = $("#public_List table tr").length;//总共有多少项
+        var number_of_pages = Math.ceil(number_of_items / show_per_page);//获得总共有多少页
+        //显示页数和每页存放的项
+        $("#pages").text(number_of_pages);
+        $("#items_of_page").text(show_per_page);
+
+        $("#current_page").val(0);
+        //循环生成页码
+        var ul_html = '<li><a class="previous_link" href="javascript:previous();">&laquo;</a></li>';
+        var current_link = 0;
+        while (number_of_pages > current_link) {
+            ul_html += '<li><a class="page_link" href="javascript:go_to_page(' + current_link + ')" longdesc="' + current_link + '">' + (current_link + 1) + '</a></li>';
+            current_link++;
+        }
+        ul_html += '<li><a class="previous_link" href="javascript:next();">&raquo;</a></li>';
+
+        $("#page_navigation").html(ul_html);//把页码放置在ul里面
+        $('#page_navigation .page_link:first').addClass('pageactive');//页面加载时为第一个页码添加pageactive样式
+        $('#public_List table tr').css('display', 'none');//先隐藏所有的项
+        $('#public_List table tr').slice(0, show_per_page).css('display', 'table-row');	//slice遍历，显示前五个段落	
+    });
+
+
+    function previous() {
+        new_page = parseInt($('#current_page').val()) - 1;
+        if ($('.pageactive').parent().prev().children('.page_link').length == true) {
+            go_to_page(new_page);
+        }
+    }
+    function next() {
+        new_page = parseInt($('#current_page').val()) + 1;
+        //if there is an item after the current active link run the function
+        if ($('.pageactive').parent().next().children('.page_link').length == true) {
+            go_to_page(new_page);
+        }
+    }
+    function go_to_page(page_num) {
+        var show_per_page = parseInt($('#items_of_page').text());//5
+        start_from = page_num * show_per_page;
+        end_on = start_from + show_per_page;
+        $('#public_List table tr').css('display', 'none').slice(start_from, end_on).css('display', 'table-row');
+        $('.page_link[longdesc=' + page_num + ']').addClass('pageactive');
+        $('.page_link[longdesc=' + page_num + ']').parent().siblings().children('.pageactive').removeClass('pageactive');
+        $('#current_page').val(page_num);
+    }
+
+
+
 
 
 
